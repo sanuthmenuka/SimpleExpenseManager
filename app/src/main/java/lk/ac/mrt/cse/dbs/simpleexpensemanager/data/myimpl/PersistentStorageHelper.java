57a84569c7171db467e0e679a11a3ae2a4a6062f
sanuthmenuka <sanuthmenuka@gmail.com>
2022-12-04 11:13:07 +0530

    Added helper and created database

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/myimpl/PersistentStorageHelper.java
@@ -0,0 +1,37 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.myimpl;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PersistentStorageHelper extends SQLiteOpenHelper {
+
+    private static final  String DB_NAME="ExpenseManager";
+    private static final int DB_VERSION=1;
+
+    //refer to constructor of SQLiteOpenHelper
+    public PersistentStorageHelper(Context context){
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+
+    //GETS CALLED WHEN DB FIRST GETS CREATED
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE accounts(AccountNo TEXT NOT NULL PRIMARY KEY,"
+                            +"BankName TEXT,"
+                            +"AccountHolder TEXT,"
+                            +"Balance REAL);");
+        sqLiteDatabase.execSQL("CREATE TABLE transactions(Date TEXT," +
+                "AccountNo TEXT," +
+                "ExpenseType TEXT," +
+                "Amount REAL," +
+                "FOREIGN KEY (AccountNo) REFERENCES accounts(AccountNo));");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
